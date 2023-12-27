174af6d37f1ca60c741696a49327e3b916ecb528
Tom Holiday <tom.holiday@mac.com>
2023-12-27 15:07:27 +0000

    christmas

--- src/Components/Board/Board.tsx
+++ src/Components/Board/Board.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import './Board.css';
-
+import React from "react";
+import "./Board.css";
 
 function Board() {
-   
+  const boardWidth: number = 6;
+  const boardHeight: number = 3;
 
   return (
     <div>
-        Hello
+      Hello
+      <div className="board-container">
+        <div className="board-outline"> </div>
+      </div>
     </div>
   );
 }
